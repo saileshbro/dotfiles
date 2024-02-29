7b670337565e71fc7b3d1fd13ff1bdb4aa989ca4
Sailesh Dahal <saileshbro@gmail.com>
2024-02-29 13:48:02 +0545

    chore: add very_good_cli completions

---
+++ .config/.dart-cli-completion/zsh-config.zsh
@@ -0,0 +1,6 @@
+
+## [very_good]
+## Completion config for "very_good"
+[[ -f /Users/saileshbro/.config/.dart-cli-completion/very_good.zsh ]] && . /Users/saileshbro/.config/.dart-cli-completion/very_good.zsh || true
+## [/very_good]
+
